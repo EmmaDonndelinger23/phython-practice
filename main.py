5c90b9576f5c1f9df9c809ce1d4c9def32694ca4
EmmaDonndelinger23 <826656@mystma.org>
2021-09-13 15:07:45 -0500

    added top 3 movies

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+print("Good Morning!")
+
+#Print first movie
+print("The Maze Runner")
+
+#Print second movie
+print("Mamma Mia")
+
+#Print third movie
+print("Guardians of the Galaxy")

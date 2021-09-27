631e20627424afacf7aa94d36e2bee59ff710bbf
EmmaDonndelinger23 <826656@mystma.org>
2021-09-27 15:14:17 -0500

    dictionary

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+def define(word):
+    word == word.lower()
+    if word == "ice cream":
+        return "a soft frozen food made with sweetened and flavored milk fat"
+    elif word == "oreo":
+        return "a brand of chocolate sandwich cookie with a creamy white filling"
+    elif word == "cookie":
+        return "a small sweet cake, typically round and flat and having a crisp or chewy texture"
+    elif word == "cake":
+        return "an item of soft, sweet food made from a mixture of flour, shortening, eggs, sugar, and other ingredients, baked and often decorated"
+    elif word == "pie":
+        return "a baked dish of fruit, or meat and vegetables, typically with a top and base of pastry"
+    elif word == "pizza":
+        return "a dish of Italian origin consisting of a flat, round base of dough baked with a topping of tomato sauce and cheese, typically with added meat or vegetables"
+    elif word == "doughnut":
+        return "a small fried cake of sweetened dough, typically in the shape of a ball or ring"
+    elif word == "cupcake":
+        return "a small cake baked in a cup-shaped container and typically iced"
+    elif word == "spaghetti":
+        return "pasta made in long, slender, solid strings"
+    elif word == "apple":
+        return "the round fruit of a tree of the rose family, which typically has thin red or green skin and crisp flesh"
+    else:
+        return "this word is not in this dictionary"
 
-def madLib(instrument, number1, day, letter, majorMinor, number2, number3, adjectice1, adjective2, fraction, adjective3, nameOfInstrument, adjective4, noun, familyMember, nameOfASong, adjective5ed, favoriteArtist):
-    print("While i've stayed at home, i've practiced the " + instrument + " for " + number1 + " hours every " + day + ". My favorite key signature to play and practice in is " + letter + majorMinor + ". It has " + number2 + " flats and" + number3 + " sharps. I like this key signature because it is " + adjectice1 + " and " + adjective2 + ". I've also gotten awesome at counting time signatures. The time signature I love is " + fraction + ", because it is " + adjective3 + " to count. One instrument I want to learn how to play while i'm at home is " + nameOfInstrument + ", because it is " + adjective4 + " and sounds like a " + noun + ". My " + familyMember + " likes it when I play" + nameOfASong + ", and always gives me a round of applause after my performances! When I return to school, my teacher will be " + adjective5ed + " of how great I am at playing my instrument. My teacher might even think I sound like " + favoriteArtist + "!")
-    
-madLib("Trumpet", "16", "Monday", "Z ", "minor", "7", " 5", "blue", "fluffy", "1/16", "sleek", "guitar", "rough", "cat", "sister", " without you", "amazed", "Ed Sheeran")
+print(define("cake"))
+print(define("banana"))
